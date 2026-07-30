ce6b024815dd146be7f409e4834498750ce787b4
chcarrie <carriechan92@gmail.com>
2026-07-30 18:00:36 -0400

    Module 1 + 2 assignments

--- Module 1/MyTestFile.cpp
+++ Module 1/MyTestFile.cpp
@@ -1,1 +1,8 @@
-cout << "This is a test file";
+#include <iostream>
+
+using namespace std;
+int main() {
+    cout <<"This is a test file for Module 1.";
+    
+    return 0;
+}
---
+++ Module 2/inventory.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    cout <<"Hello Inventory System!\t" <<;
+    cout <<"\nSize of Data Types:\n";
+    cout << "Size of int: " << sizeof(int) << " byte(s)\n";
+    cout << "Size of char: " << sizeof(char) << " bytes\n";
+    cout << "Size of float: " << sizeof(float) << " bytes\n";
+    
+    return 0;
+}
